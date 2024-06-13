8db439be60b93fce114be27cb9e87dbe3e70dbc1
Arbiter <162354595+Arbiter-289@users.noreply.github.com>
2024-06-13 12:19:22 -0400

    update purple.h

--- purple.h
+++ purple.h
@@ -2,13 +2,36 @@
 #ifndef PURPLE_H
 #define PURPLE_H
 
-void accont();
-void loggin();
-void cow();
-void menu();
-void tools();
-void mostrar();
-void bmi();
-void mostar_bmi();
+void comparar(void);
+void accont(void);
+int clave_segura(char password[20]);
+void login(void);
+char seed(void);
+void menu(void);
+void tools(void);
+void cambiar_user(void);
+void cambiar_password(void);
+void mostrar(void);
+void cow(void);
+void bmi(void);
+void anexar_bmi(void);
+void open_bmi(void);
+void comparar_bmi(void);
+void mostar_bmi(void);
+void buscar_bmi(void);
+void menu_bmi(void);
+void eliminar_bmi(void);
 float calcular_bmi(float peso, float altura);
+void stats_bmi(float bmi);
+void agregar_bmi(void);
+void edit_bmi(void);
+void limpiar_pantalla(void);
+void pause(void);
+void precionac(void);
+void invalido(void);
+void wait(void);
+void historial(void);
+void cow_menu(void);
+void show_history(void);
+void admin(void);
 #endif
